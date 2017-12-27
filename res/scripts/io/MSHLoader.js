21ffcfa4b1652e8b8b163dfb83031b5dad271be7
RepComm <info@jonathancrowder.com>
2017-12-26 19:20:49 -0700

    NAME data now terminates at 0x0 nulls, yay! DATA tag is handled, but it is also where the script messes up heiarchy.. Still work to do! Several little tags implemented
    Signed-off-by: RepComm <info@jonathancrowder.com>

--- res/scripts/io/MSHLoader.js
+++ res/scripts/io/MSHLoader.js
@@ -28,6 +28,14 @@ function MSHLoader () {
         var tagSize = buf.readInt32LE(currentOffset);
         currentOffset+=4;
         
+        /*//DEBUG
+        if (parent) {
+            console.log(currentOffset + " " + parent.type + " -> " + tagName + " with " + tagSize + " more bytes following");
+        } else {
+            console.log(currentOffset + " Top level -> " + tagName);
+        }
+        //END DEBUG*/
+        
         switch (tagName) {
             case "HEDR":
                 parent = this.finalResult;
@@ -64,7 +72,17 @@ function MSHLoader () {
                 }
                 break;
             case "NAME":
-                parent[tagName] = buf.toString("utf8", currentOffset, currentOffset+tagSize);
+                //var str = buf.toString("utf8", currentOffset, currentOffset+tagSize);
+                var str = "";
+                var trim = buf.slice(currentOffset, currentOffset+tagSize);
+                for (let i=0; i<trim.length; i++) {
+                    if (trim[i] == 0x0) { //Forget about padded 0x0 (null) bytes
+                        break;
+                    }
+                    str+=String.fromCharCode(trim[i]);
+                }
+                
+                parent[tagName] = str;
                 currentOffset+=tagSize;
                 currentOffset +=this.readTag(parent, buf, currentOffset, leveldeep, childCount);
                 break;
@@ -132,20 +150,139 @@ function MSHLoader () {
                     childCount++;
                 }
                 break;
-                break;
-            /*case "DATA": //Thats enough for tonight.. Merry christmas!
-                //TODO ----------------- VARIES BASED ON PARENT TAG (good thing we keep track of that, huh?)
+            case "DATA":
+                console.log(leveldeep + " " + parent.type + " > " + tagName);
                 switch(parent.type) {
-                    //Do stuff different for each type of parent of DATA tag (see [info].msh structure notes)
+                    case "CAMR":
+                        var c = parent[tagName] = {
+                            type:tagName,
+                            floats:[]
+                        };
+                        //UNDOCUMENTED VALUES, we'll have to use XSI to mess with them until we get it later
+                        c.floats[0] = buf.readFloatLE(currentOffset); currentOffset+=4;
+                        c.floats[1] = buf.readFloatLE(currentOffset); currentOffset+=4;
+                        c.floats[2] = buf.readFloatLE(currentOffset); currentOffset+=4;
+                        
+                        c.floats[3] = buf.readFloatLE(currentOffset); currentOffset+=4;
+                        c.floats[4] = buf.readFloatLE(currentOffset); currentOffset+=4;
+                        c.floats[5] = buf.readFloatLE(currentOffset); currentOffset+=4;
+                        
+                        c.floats[6] = buf.readFloatLE(currentOffset); currentOffset+=4;
+                        c.floats[7] = buf.readFloatLE(currentOffset); currentOffset+=4;
+                        c.near = buf.readFloatLE(currentOffset); currentOffset+=4;
+                        c.far = buf.readFloatLE(currentOffset); currentOffset+=4;
+                        currentOffset +=this.readTag(parent, buf, currentOffset, leveldeep, childCount);
+                        break;
+                    case "LGTI": //NOT TESTED
+                        var c = parent[tagName] = {
+                            type:tagName,
+                            floats:[]
+                        };
+                        //UNDOCUMENTED VALUES, we'll have to use XSI to mess with them until we get it later
+                        c.floats[0] = buf.readFloatLE(currentOffset); currentOffset+=4;
+                        c.floats[1] = buf.readFloatLE(currentOffset); currentOffset+=4;
+                        c.floats[2] = buf.readFloatLE(currentOffset); currentOffset+=4;
+                        
+                        c.floats[3] = buf.readFloatLE(currentOffset); currentOffset+=4;
+                        c.floats[4] = buf.readFloatLE(currentOffset); currentOffset+=4;
+                        c.floats[5] = buf.readFloatLE(currentOffset); currentOffset+=4;
+                        
+                        c.floats[6] = buf.readFloatLE(currentOffset); currentOffset+=4;
+                        c.floats[7] = buf.readFloatLE(currentOffset); currentOffset+=4;
+                        c.floats[8] = buf.readFloatLE(currentOffset); currentOffset+=4;
+                        currentOffset +=this.readTag(parent, buf, currentOffset, leveldeep, childCount);
+                        break;
+                    case "MATD": //NOT TESTED
+                        var c = parent[tagName] = {
+                            type:tagName,
+                            diffuseColor:[],
+                            ambientColor:[],
+                            specularColor:[],
+                            specularIntensity:0
+                        };
+                        //UNDOCUMENTED VALUES, we'll have to use XSI to mess with them until we get it later
+                        c.diffuseColor[0] = buf.readFloatLE(currentOffset); currentOffset+=4;
+                        c.diffuseColor[1] = buf.readFloatLE(currentOffset); currentOffset+=4;
+                        c.diffuseColor[2] = buf.readFloatLE(currentOffset); currentOffset+=4;
+                        c.diffuseColor[3] = buf.readFloatLE(currentOffset); currentOffset+=4;
+                        
+                        c.ambientColor[0] = buf.readFloatLE(currentOffset); currentOffset+=4;
+                        c.ambientColor[1] = buf.readFloatLE(currentOffset); currentOffset+=4;
+                        c.ambientColor[2] = buf.readFloatLE(currentOffset); currentOffset+=4;
+                        c.ambientColor[3] = buf.readFloatLE(currentOffset); currentOffset+=4;
+                        
+                        c.specularColor[0] = buf.readFloatLE(currentOffset); currentOffset+=4;
+                        c.specularColor[1] = buf.readFloatLE(currentOffset); currentOffset+=4;
+                        c.specularColor[2] = buf.readFloatLE(currentOffset); currentOffset+=4;
+                        c.specularColor[3] = buf.readFloatLE(currentOffset); currentOffset+=4;
+                        
+                        c.specularIntensity = buf.readFloatLE(currentOffset); currentOffset+=4;
+                        
+                        currentOffset +=this.readTag(parent, buf, currentOffset, leveldeep, childCount);
+                        break;
+                    default:
+                        leveldeep++;
+                        parent[tagName + "_" + levelwidth] = {
+                            type:tagName,
+                            contentSize:tagSize,
+                            unhandled: true
+                        };
+                        
+                        currentOffset += tagSize;
+                        currentOffset +=this.readTag(parent, buf, currentOffset, leveldeep, childCount);
+                        break;
                 }
+                break;
+            /*case "ANIM": //Can't test this yet
+                
                 break;*/
+            case "LGTI": //NOT TESTED
+                var childCount = 0;
+                leveldeep++;
+                var p = parent[tagName] = {
+                    type:tagName,
+                    contentSize:tagSize
+                };
+                for (var i=currentOffset-start; i<tagSize;i=currentOffset) {
+                    currentOffset += this.readTag(p, buf, currentOffset, leveldeep, childCount);
+                    childCount++;
+                }
+                break;
+            case "MATL":
+                var childCount = 0;
+                leveldeep++;
+                var p = parent[tagName] = {
+                    type:tagName,
+                    contentSize:tagSize,
+                    materialCount:0
+                };
+                p.materialCount = buf.readInt32LE(currentOffset); currentOffset+=4;
+                
+                for (var i=currentOffset-start; i<tagSize;i=currentOffset) {
+                    currentOffset += this.readTag(p, buf, currentOffset, leveldeep, childCount);
+                    childCount++;
+                }
+                break;
+            case "MATD":
+                var childCount = 0;
+                leveldeep++;
+                var p = parent[tagName + "_" + levelwidth] = {
+                    type:tagName,
+                    contentSize:tagSize
+                };
+                
+                for (var i=currentOffset-start; i<tagSize;i=currentOffset) {
+                    currentOffset += this.readTag(p, buf, currentOffset, leveldeep, childCount);
+                    childCount++;
+                }
+                break;
             case "CL1L":
                 console.log("CL1L has " + tagSize + " bytes following, usually EOF");
                 this.reachedEofSafely = true;
                 break;
             default:
                 leveldeep++;
-                var p = parent[tagName + "_" + levelwidth] = {
+                parent[tagName + "_" + levelwidth] = {
                     type:tagName,
                     contentSize:tagSize,
                     unhandled: true
